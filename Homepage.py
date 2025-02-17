9c4a04edc0199b92aa439503196f26f93611cc64
handoverhandprompting <150869385+handoverhandprompting@users.noreply.github.com>
2025-02-17 10:17:00 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -24,18 +24,8 @@ def run():
                 elif st.session_state['_language'] == 'english':
                     st.session_state['_language'] = 'chinese'
 
-    st.header('國立臺灣大學醫學院附設醫院 心臟衰竭藥事照護服務
-')
-    st.write('- 您知道為什麼要吃這些藥嗎？ -
-- 您對於藥品有很多疑問嗎？ -
-- 您想要看懂自己的抽血報告嗎？ -
-- 您希望更瞭解自己目前心臟衰竭的情況嗎？ -
-
-您的需求，我們都聽到了
-藥師通通都可以耐心為您解答
-
-有了這個互動式數位工具之後，您可以用更簡單的方式來瞭解這些問題
-')
+    st.header('National Taiwan University Hospital Pharmacist-Led Heart Failure Clinic')
+    st.write('content')
 
 if __name__ == '__main__':
     run()
